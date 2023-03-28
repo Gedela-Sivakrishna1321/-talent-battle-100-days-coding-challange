15799dd50ddcbcca596e71f93d9ec680389ea33a
sivakrishna <gedelashipa.1321@gmail.com>
2023-03-28 20:49:00 +0530

    Day 26

---
+++ Sivakrishna_Day26.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+public class Sivakrishna_Day26 {
+	public static void main(String args[]) {
+	Scanner sc = new Scanner(System.in);
+	System.out.print("Input = ");
+	int num_people = sc.nextInt();
+	// Sum of first n-1 terms = {(n-1)/2}*n;
+	int No_handshakes = ((num_people-1)*num_people)/2; 
+	System.out.println(No_handshakes);
+	sc.close();
+	}
+}
