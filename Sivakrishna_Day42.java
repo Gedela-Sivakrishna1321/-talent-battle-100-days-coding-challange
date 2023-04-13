ebe2bb9091339d4a1ab6942c439e12dd5ddb68dc
sivakrishna <gedelashipa.1321@gmail.com>
2023-04-13 21:36:01 +0530

    Updated

--- Sivakrishna_Day42.java
+++ Sivakrishna_Day42.java
@@ -2,8 +2,12 @@ import java.util.Scanner;
 public class Sivakrishna_Day42 {
   public static void main(String args[]) {
     Scanner sc = new Scanner(System.in);
-    int arr1[] = new int[5];
-    int arr2[] = new int[5];
+    System.out.print("Enter the size of Array1 : ");
+    int size1 = sc.nextInt();
+    System.out.print("Enter the size of Array2 : ");
+    int size2 = sc.nextInt();
+    int arr1[] = new int[size1];
+    int arr2[] = new int[size2];
     System.out.println("Enter arr1 elements : ");
     for(int i=0;i<arr1.length;i++) {
         arr1[i] = sc.nextInt();
@@ -21,6 +25,9 @@ public class Sivakrishna_Day42 {
   }  
 
  static boolean Isequal(int[] arr1,int[] arr2) {
+  if(arr1.length != arr2.length) {
+    return false;
+  }
     for(int i=0;i<arr1.length;i++) {
         if(arr1[i] != arr2[i]) 
             return false;
