4c0e7bab7306ca4ed5db1883f52391d68eafa659
sivakrishna <gedelashipa.1321@gmail.com>
2023-05-28 19:44:25 +0530

    Day 82

--- Codechef.java
+++ Codechef.java
@@ -1,22 +1,20 @@
-import java.io.BufferedReader;
-import java.io.InputStreamReader;
-import java.util.Arrays;
 
+import java.util.Scanner;
 public class Codechef {
-	public static void main(String[] args) throws java.lang.Exception {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		int t = Integer.parseInt(br.readLine());
-		while (t-- > 0) {
-			int n = Integer.parseInt(br.readLine());
-			String s = br.readLine();
-			String sr[] = s.split(" ");
-			int ar[] = new int[n];
-			for (int i = 0; i < n; i++)
-				ar[i] = Integer.parseInt(sr[i]);
-			Arrays.sort(ar);
-			long cout = Integer.MIN_VALUE;
-			cout = (long) (ar[n - 1] - ar[0]) * ar[n - 2];
-			System.out.println(cout);
+	public static void main(String[] args)  {
+	Scanner sc = new Scanner(System.in);
+	int t = sc.nextInt();
+	while(t-- >0) {
+		int N = sc.nextInt();
+		String ans = "";
+		for(int i=0;i<N;i++) {
+			String str = sc.next();
+			if(str.charAt(i) == '1')
+				ans += 0;
+			else 
+				ans += 1;
 		}
+		System.out.println(ans);
+	}		
 	}
 }
---
+++ Sivakrishna_Day82.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+public class Sivakrishna_Day82 {
+    public static void main(String[] args) {
+    Scanner sc = new Scanner(System.in);
+    int t = sc.nextInt();
+    while(t-- >0) {
+        int N = sc.nextInt();
+		String ans = "";
+		for(int i=0;i<N;i++) {
+			String str = sc.next();
+			if(str.charAt(i) == '1')
+				ans += 0;
+			else 
+				ans += 1;
+		}
+		System.out.println(ans);
+    }        
+    sc.close();
+    }
+}
